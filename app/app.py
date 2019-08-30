7545a7d13a3bf9a5bb35191a27429b33cb1969ae
Dalton Barreto <daltonmatos@gmail.com>
2019-08-29 21:45:38 -0300

    Implementa a possibilidade de vincular um StatusCode com um Resource
    Cada resource retornado pelo handler pode escolher qual status_code será
    colocado na Response. Isso é feito com o tipo genérico
    `HTTP[Status, Resource]`.

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
-from functools import partialmethod, partial
+from functools import partial
+from typing import Union
 
 from aiohttp import web
 
 from app.content import content_negotiation
-from app.resources.base import Resource
+from app.http.status import Status
 from app.resources.user import UserResource
+from app.types.http import HTTP
 from asyncworker import App, RouteTypes
 
 app = App()
@@ -23,3 +25,9 @@ async def handler():
 @content_negotiation
 async def users() -> UserResource:
     return UserResource(id=1, name="John Doe", phone="+5521...")
+
+
+@app.http(["/users/2"], methods=["GET"])
+@content_negotiation
+async def users_2() -> HTTP[Status.ACCEPTED, UserResource]:
+    return UserResource(id=2, name="Other User", phone="+5511...")
